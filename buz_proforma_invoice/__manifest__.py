7af890fe56d27c50682892a9744be8f660c4c312
apcball <apichart.apcball@gmail.com>
2025-02-22 06:47:22 +0000

    Update Module

---
+++ buz_proforma_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Buz Proforma Invoice',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Module to manage proforma invoices',
+    'description': """
+    This module adds the functionality to create and print proforma invoices based on sale orders.
+    """,
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['base', 'sale', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/sale_order_views.xml',
+        'reports/proforma_invoice_report.xml',
+    ],
+    'assets': {
+        'web.report_assets_common': [
+            'buz_proforma_invoice/static/src/scss/proforma_invoice_style.scss',
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
