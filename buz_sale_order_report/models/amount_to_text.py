f7ff0396edea924b0b1eb759af7f1caa80a8c786
apcball <apichart.apcball@gmail.com>
2025-02-22 08:41:53 +0000

    update code

--- buz_sale_order_report/models/amount_to_text.py
+++ buz_sale_order_report/models/amount_to_text.py
@@ -1,5 +1,6 @@
 from odoo import models, api
 from bahttext import bahttext
+from num2words import num2words
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -10,4 +11,20 @@ class SaleOrder(models.Model):
 
     def amount_to_text_en(self):
         """Convert amount to English text"""
-        # ... existing English conversion code ...
+        amount_in_words = num2words(self.amount_total, lang='en')
+        currency_name = self.currency_id.name or ''
+
+        # Format the first letter of each word to uppercase and add currency
+        amount_text = amount_in_words.title()
+
+        # Handle decimal part
+        amount_float = self.amount_total
+        decimal_part = round((amount_float - int(amount_float)) * 100)
+
+        if decimal_part > 0:
+            decimal_words = num2words(decimal_part, lang='en').title()
+            final_text = f"{amount_text} {currency_name} and {decimal_words} Cents Only"
+        else:
+            final_text = f"{amount_text} {currency_name} Only"
+
+        return final_text
