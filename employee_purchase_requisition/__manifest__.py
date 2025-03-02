af39c39d6d308403bbe48c1d3d2b649f3aa0af42
apcball <apichart.apcball@gmail.com>
2025-03-02 05:53:50 +0000

    Update module and report

--- employee_purchase_requisition/__manifest__.py
+++ employee_purchase_requisition/__manifest__.py
@@ -1,24 +1,3 @@
-# -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#
-#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
-#    Author: Ashok PK (odoo@cybrosys.com)
-#
-#    You can modify it under the terms of the GNU AFFERO
-#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
-#
-#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
-#    (AGPL v3) along with this program.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
 {
     'name': 'buz Employee Purchase Requisition',
     'version': '17.0.1.0.0',
@@ -38,6 +17,7 @@
         'security/employee_purchase_requisition_security.xml',
         'security/ir.model.access.csv',
         'data/ir_sequence_data.xml',
+        'data/system_parameter.xml',
         'views/employee_purchase_requisition_views.xml',
         'views/requisition_order_views.xml',
         'views/employee_purchase_requisition_menu.xml',
