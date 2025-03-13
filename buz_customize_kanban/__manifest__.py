c5a9f0dcebf69ff8c727645cfc6d18d20dcc7310
apcball <apichart.apcball@gmail.com>
2025-03-13 03:59:30 +0000

    Update Module

---
+++ buz_customize_kanban/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Product Kanban Customization',
+    'version': '17.0.1.0.0',
+    'category': 'Sales',
+    'depends': ['product'],
+    'data': [
+        'views/product_views.xml',
+    ],
+    'license': 'LGPL-3',
+}
