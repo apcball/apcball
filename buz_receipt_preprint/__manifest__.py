9f492bff98a8839f30e2b7b491fb8df7d561e7cf
apcball <apichart.apcball@gmail.com>
2025-03-25 09:54:53 +0000

    ok

---
+++ buz_receipt_preprint/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Pre-printed Receipt',
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Pre-printed Receipt Form',
+    'description': """
+        This module adds support for printing receipts on pre-printed forms.
+        Features:
+        - Custom receipt layout for pre-printed forms
+        - Configurable receipt format
+        - Support for Thai language
+    """,
+    'author': 'Your Company',
+    'website': 'https://www.yourcompany.com',
+    'depends': ['base', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/receipt_views.xml',
+        'reports/receipt_report.xml',
+        'reports/receipt_templates.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
