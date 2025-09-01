9ad69267d5d81839ef97ed3f21fb10ff2e1fab1d
apcball <apichart.apcball@gmail.com>
2025-09-01 08:40:42 +0000

    ok

--- marketplace_settlement/__manifest__.py
+++ marketplace_settlement/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "Marketplace Settlement (Shopee/Lazada)",
+    "name": "Marketplace Settlement",
     "summary": "Wizard to group many customer invoices into a single receivable to marketplace (e.g., Shopee)",
     "version": "17.0.1.0.0",
     "author": "Ball + Manow",
