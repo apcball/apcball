800240a8fe1f5896790af1b8bf7e3442cad912b1
apcball <apichart.apcball@gmail.com>
2025-09-18 04:33:10 +0000

    ok

--- l10n_th_account_tax_expense/__manifest__.py
+++ l10n_th_account_tax_expense/__manifest__.py
@@ -1,18 +1,23 @@
-# Copyright 2024 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# Copyright 2020 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Thai Localization - Expense Tax Invoice",
-    "version": "17.0.1.0.2",
+    "name": "Thai Localization - Expense Tax",
+    "version": "17.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-thailand",
     "category": "Localization / Accounting",
-    "depends": ["hr_expense", "l10n_th_account_tax"],
+    "depends": [
+        "l10n_th_account_tax",
+        "hr_expense_advance_clearing",
+    ],
     "data": [
-        "views/hr_expense_views.xml",
+        "views/hr_expense_view.xml",
+        "wizard/account_payment_register_views.xml",
     ],
     "installable": True,
+    "auto_install": False,
     "development_status": "Beta",
     "maintainers": ["kittiu"],
 }
