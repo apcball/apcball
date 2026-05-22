77c859d6d3a2293381cc3e99c27a73fe9c38b84b
APCBALL <apichart.apcball@gmail.com>
2026-05-22 16:21:12 +0700

    test: add comprehensive tests for budget_engine_core and biz_monthly_analytic_budget

--- biz_monthly_analytic_budget/tests/__init__.py
+++ biz_monthly_analytic_budget/tests/__init__.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from . import common
-from . import test_account_move_monthly_budget
+from . import test_budget_engine
+from . import test_budget_commitment
 from . import test_monthly_budget_lifecycle
 from . import test_purchase_order_monthly_budget
 from . import test_purchase_requisition_monthly_budget
-from . import test_monthly_budget_report
-from . import test_expected_payment_sync
+from . import test_account_move_monthly_budget
 from . import test_budget_approval_request
+from . import test_expected_payment_sync
+from . import test_monthly_budget_report
+from . import test_plan_lifecycle
+from . import test_budget_line_constraints
+from . import test_fixed_cost
