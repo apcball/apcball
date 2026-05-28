87c5b305500b71d440e56122d5b476ad3424ff26
APCBALL <apichart.apcball@gmail.com>
2026-05-28 20:00:59 +0700

    ok

--- pos_lite/report/receipt_report.py
+++ pos_lite/report/receipt_report.py
@@ -16,6 +16,28 @@ class ReportPosLiteReceipt(models.AbstractModel):
         }
 
 
+class ReportPosLiteInvoice(models.AbstractModel):
+    _name = 'report.pos_lite.report_pos_lite_invoice'
+    _description = 'POS Lite Invoice Report (via account.move)'
+
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        orders = self.env['pos.lite.order'].browse(docids)
+        invoices = orders.mapped('invoice_id')
+        # Map invoice -> POS employee name for report display
+        pos_employee_map = {}
+        for order in orders:
+            if order.invoice_id and order.employee_id:
+                pos_employee_map[order.invoice_id.id] = order.employee_id.name
+        return {
+            'doc_ids': invoices.ids,
+            'doc_model': 'account.move',
+            'docs': invoices,
+            'data': data or {},
+            'pos_employee_map': pos_employee_map,
+        }
+
+
 class ReportPosLiteSession(models.AbstractModel):
     _name = 'report.pos_lite.report_session_document'
     _description = 'POS Lite Session Summary Report'
