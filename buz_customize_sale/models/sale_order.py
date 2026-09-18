ae757982026bef74baafb0e88e4ce94f07483b16
Golf-it <teerapon.s@mogen.co.th>
2026-09-18 12:03:50 +0700

    feat: add hide unlock and cancel buttons functionality for sale orders based on user permissions

--- buz_customize_sale/models/sale_order.py
+++ buz_customize_sale/models/sale_order.py
@@ -1,8 +1,13 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    hide_unlock_cancel_buttons = fields.Boolean(
+        compute="_compute_hide_unlock_cancel_buttons",
+        help="Technical field for hiding Unlock and Cancel buttons for selected users.",
+    )
 
     approver_id = fields.Many2one(
         "res.users",
@@ -17,3 +22,14 @@ class SaleOrder(models.Model):
         string="Signature",
         readonly=True,
     )
+
+    @api.depends("delivery_count")
+    @api.depends_context("uid")
+    def _compute_hide_unlock_cancel_buttons(self):
+        user_can_hide_buttons = self.env.user.has_group(
+            "buz_customize_sale.group_hide_so_unlock"
+        )
+        for order in self:
+            order.hide_unlock_cancel_buttons = bool(
+                user_can_hide_buttons and order.delivery_count
+            )
